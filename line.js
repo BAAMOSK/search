f1611850143c35f50e1bae99f64ae6a120cae475
htwright <harrisontwright@gmail.com>
2017-06-28 11:49:33 -0500

    first three drills done

--- line.js
+++ line.js
@@ -1,35 +1,65 @@
 class BinarySearchTree {
-    constructor(key=null, value=null, parent=null) {
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
 
-    insert(key, value) {
-        if (this.key == null) {
-            this.key = key;
-            this.value = value;
-        }
-        else if (key < this.key) {
-            if (this.left == null) {
-                this.left = new BinarySearchTree(key, value, this);
-            }
-            else {
-                this.left.insert(key, value);
-            }
-        }
-        else {
-            if (this.right == null) {
-                this.right = new BinarySearchTree(key, value, this);
-            }
-            else {
-                this.right.insert(key, value);
-            }
-        }
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
     }
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+  dfs(values = []) {
+    if (this.left) {
+      values = this.left.dfs(values);
+    }
+    values.push(this.value);
+    if (this.right) {
+      values = this.right.dfs(values);
+    }
+    return values;
+  }
+  //breadth first goes through each level --left to right
+  //builds a queue at each level
+  bfs(values = []) {
+    const queue = [this];
+    while(queue.length) {
+      const node = queue.shift();
+      values.push(node.value);
+      if(node.left) {
+        queue.push(node.left);
+      }
+      if(node.right){
+        queue.push(node.right);
+      }
+    }
+    return values;
+  }
+
 }
+
+
+
 
 function indexOf(array, value) {
   for(let i=0; i<array.length; i++) {
@@ -38,15 +68,14 @@ function indexOf(array, value) {
     }
   }
   return -1;
-};
+}
 
 const letters = ['a','b','c','d'];
 
-// console.log(indexOf(letters, 'd'));
 
-function binarySearch(array, value, start, end) {
-  start = start === undefined ? 0 : start;
-  end = end === undefined ? array.length : end;
+function binarySearch(array, value, start = 0, end = array.length) {
+  // start = start === undefined ? 0 : start;
+  // end = end === undefined ? array.length : end;
 
   if(start > end) {
     return -1;
@@ -54,7 +83,6 @@ function binarySearch(array, value, start, end) {
 
   const index = Math.floor((start + end) / 2);
   const item = array[index];
-  console.log(start, end);
 
   if(item === value) {
     return index;
@@ -67,39 +95,38 @@ function binarySearch(array, value, start, end) {
   }
 }
 
-// console.log(binarySearch(letters, 'd'));
-
 class BinaryTree {
-  constructor(value) {
+  constructor(key = null, value = null, parent=null) {
+    this.key = key;
     this.value = value;
+    this.parent = parent;
     this.left = null;
     this.right = null;
   }
   insert(key, value) {
-        if (this.key == null) {
-            this.key = key;
-            this.value = value;
-        }
-        else if (key < this.key) {
-            if (this.left == null) {
-                this.left = new BinarySearchTree(key, value, this);
-            }
-            else {
-                this.left.insert(key, value);
-            }
-        }
-        else {
-            if (this.right == null) {
-                this.right = new BinarySearchTree(key, value, this);
-            }
-            else {
-                this.right.insert(key, value);
-            }
-        }
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
     }
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinaryTree(key, value, this);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right === null) {
+        this.right = new BinaryTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
   //depth first goes all the way to bottom
   dfs(values = []) {
-    console.log(`This is the  dfs this`, this);
     if (this.left) {
       values = this.left.dfs(values);
     }
@@ -107,27 +134,86 @@ class BinaryTree {
     if (this.right) {
       values = this.right.dfs(values);
     }
-    console.log(value);
     return values;
   }
   //breadth first goes through each level --left to right
-  bfs(value = []) {
+  bfs(values = []) {
     const queue = [this];
     while(queue.length) {
       const node = queue.shift();
       values.push(node.value);
       if(node.left) {
-        queue.push()
+        queue.push();
       }
     }
   }
 }
 
-const Btree = new BinaryTree();
-Btree.insert(3, 3);
-Btree.insert(5, 5);
-Btree.insert(8, 8);
-Btree.insert(1, 1);
-Btree.insert(2, 2);
 
-console.log(Btree.dfs());
+let input = [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68,
+  15, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 31, 
+  26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5, 76, 62];
+
+function intSearch(array, int){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === int){
+      return array[i];
+    }
+  }
+  return -1;
+}
+
+// console.log(intSearch(input, 0));
+// let input1 = input.sort((a, b) => a - b);
+// console.log(input1);
+// console.log(binarySearch(input1, 0));
+
+// const Btree = new BinaryTree();
+// Btree.insert(5, 5);
+// Btree.insert(3, 3);
+// Btree.insert(8, 8);
+// Btree.insert(1, 1);
+// Btree.insert(2, 2);
+
+// console.log(Btree);
+
+
+//given 531.45
+//climb through a BST looking for the correct sequence of numbers
+//on the first recursion, look for the first number in the int.
+//then call the function again with (tree.left || tree.right, next number)
+
+
+//walk into library, looking for histry book 831.32
+//go to shelf with 500 category. Go to the higher end. This effectively eliminates half of the library from consideration.
+//repeat this process until we narrow it down to the book we're looking for.
+
+let library = [100, 200, 300, 400, 500, 562.3, 600, 700, 800, 900];
+
+function deweyDecimal(library, target, start = 0, end = library.length){
+  if(start > end){
+    return -1;
+  }
+  let midpoint = Math.floor((start + end) / 2);
+  console.log(midpoint + '<--  midpoint   ' + start + '  <---- start' + end + '  <-----end' );
+  if(library[midpoint] > target){
+    return deweyDecimal(library, target, start, midpoint - 1);
+  }else if (library[midpoint] < target){
+    return deweyDecimal(library, target, midpoint + 1, end);
+  } else {
+    return midpoint;
+  }
+}
+let i = deweyDecimal(library, 200);
+console.log(i);
+
+// const Btree = new BinaryTree();
+// Btree.insert(5, 5);
+// Btree.insert(3, 3);
+// Btree.insert(1, 1);
+// Btree.insert(2, 2);
+// Btree.insert(5, 5);
+
+// // console.log(BStree.dfs());
+// console.log(deweyDecimal(Btree, 531.45));
+// console.log(Btree);
